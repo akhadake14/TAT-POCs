304d54772c87fada14a0de03522659c747eb32a0
Abhijeet Khadake <akhadake14@users.noreply.github.com>
2021-12-20 21:44:09 +0530

    Repository moved

---
+++ gRPC POC/NOV.TAT.ProductgRPC.Data/Repository/ProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using NOV.TAT.ProductgRPC.Business.Models;
+using NOV.TAT.ProductgRPC.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NOV.TAT.ProductgRPC.Data
+{
+    public class ProductRepository : GenericRepository<Product, ProductContext>
+    {
+        public ProductRepository(ProductContext _context) : base(_context)
+        {
+        }
+    }
+}
