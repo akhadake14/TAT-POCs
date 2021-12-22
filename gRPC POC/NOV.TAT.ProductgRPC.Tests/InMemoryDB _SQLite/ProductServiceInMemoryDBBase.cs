d6a6573bca53f34139a7bd1c541eae62f28f7f6d
Abhijeet Khadake <akhadake14@users.noreply.github.com>
2021-12-22 18:44:26 +0530

    API testing with in memory database and SQLite

---
+++ gRPC POC/NOV.TAT.ProductgRPC.Tests/InMemoryDB _SQLite/ProductServiceInMemoryDBBase.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using NOV.TAT.ProductgRPC.Business.Models;
+using NOV.TAT.ProductgRPC.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NOV.TAT.ProductgRPC.Tests.InMemoryDB__SQLite
+{
+    public  class ProductServiceInMemoryDBBase : IDisposable
+    {
+        public ProductContext context { get; set; }
+
+        public ProductServiceInMemoryDBBase()
+        {
+            context = CreateContextForInMemory();
+        }
+        public ProductContext CreateContextForInMemory()
+        {
+            var option = new DbContextOptionsBuilder<ProductContext>().UseInMemoryDatabase(databaseName: "Test_Database").Options;
+
+            context = new ProductContext(option);
+            Seed(context);
+            return context;
+        }
+
+        public ProductContext CreateContextForSQLite()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+            var option = new DbContextOptionsBuilder<ProductContext>().UseSqlite(connection).Options;
+
+            context = new ProductContext(option);
+            Seed(context);
+            return context;
+
+        }
+
+        private void Seed(ProductContext context)
+        {
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+            var data = new List<Product>
+            {
+                new Product {Id=1, Name = "Product1" ,Description="Description1", UnitPrice=11.11f},
+                 new Product {Id=2, Name = "Product2" ,Description="Description2", UnitPrice=21.11f},
+                 new Product {Id=3, Name = "Product3" ,Description="Description3", UnitPrice=31.11f},
+            }.AsQueryable();
+
+            context.Products.AddRange(data);
+            context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+           context.Dispose();
+        }        
+    }
+}
---
+++ gRPC POC/NOV.TAT.ProductgRPC.Tests/InMemoryDB _SQLite/ProductServiceSQLiteDBTests.cs
@@ -0,0 +1,167 @@
+﻿using AutoMapper;
+using Grpc.Core;
+using Grpc.Core.Testing;
+using Grpc.Core.Utils;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NOV.TAT.ProductgRPC.Data;
+using NOV.TAT.ProductgRPC.Service.Mapper;
+using NOV.TAT.ProductgRPC.Service.Services;
+using ProductGRPCService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NOV.TAT.ProductgRPC.Tests.InMemoryDB__SQLite
+{
+    [TestClass]
+    public class ProductServiceSQLiteDBTests :ProductServiceInMemoryDBBase
+    {
+        private ProductService productservice;
+        private IMapper mapper;
+        private ILogger<ProductService> logger;
+        private ServerCallContext serverCallContext;
+        public ProductServiceSQLiteDBTests() : base()
+        {
+
+        }
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            var myProfile = new ProductProfile();
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(myProfile));
+            mapper = new Mapper(configuration);
+
+            var mockLogger = new Mock<ILogger<ProductService>>();
+            logger = mockLogger.Object;
+            serverCallContext = GetTestServerCallContext();
+
+        }
+
+        [TestMethod]
+        public void AddProductTest()
+        {
+            using (context = CreateContextForSQLite())
+            {
+                productservice = new ProductService(new ProductRepository(context), mapper, logger);
+
+                var productResponse = productservice.AddProduct(new ProductRequest()
+                {
+                    Product = new ProductModel()
+                    {
+                        Id = 5,
+                        Name = "Test",
+                        Description = "DescriptionTest",
+                        UnitPrice = 55.8f
+                    }
+                }, serverCallContext);
+
+                Assert.AreEqual((int)ResponseStatusCode.Ok, productResponse.Result.StatusCode);
+                Assert.AreEqual(ResponseStatusCode.Created.ToString(), productResponse.Result.Description);
+            }
+        }
+        [TestMethod]
+        public void GetAllProductTest()
+        {
+            using (context = CreateContextForSQLite())
+            {
+                productservice = new ProductService(new ProductRepository(context), mapper, logger);
+
+                var productResponse = productservice.GetProduts(new ProductRequest(), serverCallContext);
+
+                Assert.AreEqual(3, productResponse.Result.Products.Count);
+                Assert.AreEqual(1, productResponse.Result.Products[0].Id);
+                Assert.AreEqual("Product2", productResponse.Result.Products[1].Name);
+                Assert.AreEqual("Description3", productResponse.Result.Products[2].Description);
+            }
+        }
+
+        [TestMethod]
+        public void GetbyIDTest()
+        {
+            using (context = CreateContextForSQLite())
+            {
+                productservice = new ProductService(new ProductRepository(context), mapper, logger);
+
+                var productResponse = productservice.Get(new ProductRequest()
+                {
+                    Product = new ProductModel()
+                    {
+                        Id = 2
+                    }
+                }, serverCallContext);
+
+                Assert.AreEqual(2, productResponse.Result.Product.Id);
+                Assert.AreEqual("Product2", productResponse.Result.Product.Name);
+                Assert.AreEqual("Description2", productResponse.Result.Product.Description);
+            }
+        }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            using (context = CreateContextForSQLite())
+            {
+                productservice = new ProductService(new ProductRepository(context), mapper, logger);
+
+                var productResponse = productservice.UpdateProduct(new ProductRequest()
+                {
+                    Product = new ProductModel()
+                    {
+                        Id = 2,
+                        Name = "Updated Name",
+                        Description = "Updated dis",
+                        UnitPrice = 787.5f
+                    }
+                }, serverCallContext);
+
+                Assert.AreEqual((int)ResponseStatusCode.Ok, productResponse.Result.StatusCode);
+                Assert.AreEqual(ResponseStatusCode.Updated.ToString(), productResponse.Result.Description);
+                Assert.AreEqual(2, productResponse.Result.Product.Id);
+                Assert.AreEqual("Updated Name", productResponse.Result.Product.Name);
+            }
+        }
+
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            using (context = CreateContextForSQLite())
+            {
+
+                productservice = new ProductService(new ProductRepository(context), mapper, logger);
+
+                var productResponse = productservice.DeleteProduct(new ProductRequest()
+                {
+                    Product = new ProductModel()
+                    {
+                        Id = 2
+                    }
+                }, serverCallContext);
+
+                Assert.AreEqual((int)ResponseStatusCode.Ok, productResponse.Result.StatusCode);
+                Assert.AreEqual(ResponseStatusCode.Deleted.ToString(), productResponse.Result.Description);
+            }
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+        }
+
+        #region Private methods 
+        private ServerCallContext GetTestServerCallContext()
+        {
+            return TestServerCallContext.Create
+                            ("", null, DateTime.UtcNow.AddHours(1), new Metadata(), CancellationToken.None, "", null, null,
+                            (metadata) => TaskUtils.CompletedTask, () => new WriteOptions(), (writeOptions) => { });
+        }
+
+        #endregion
+    }
+}
